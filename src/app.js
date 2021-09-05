6863c467a018ddb81557fac19a2b4933e1a5f670
Rody11 <84354563+Rody11@users.noreply.github.com>
2021-09-05 15:46:28 -0500

    Mostrar datos en navegador.

---
+++ src/app.js
@@ -0,0 +1,16 @@
+import express, { response } from 'express'
+import morgan from 'morgan'
+
+const app = express()
+
+app.use(morgan('dev'));
+
+app.get('/', (req, res) =>{
+    res.json({
+        autor: 'Rody',
+        descripcion: "Proyecto autenticacion",
+        version: "1.0.0"
+    })
+})
+
+export default app;
