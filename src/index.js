160e80868e17dfbd6b371beb02297b52d44ac540
Rody11 <84354563+Rody11@users.noreply.github.com>
2021-09-05 22:05:38 -0500

    Cambio de puerto

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import app from './app'
 import './database'
 
-app.listen(3000);
+app.listen(4000);
 
-console.log('El servidor esta escuchando en el puerto:',3000)
+console.log('El servidor esta escuchando en el puerto:',4000)
 
